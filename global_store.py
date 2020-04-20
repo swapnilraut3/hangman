4926379882fedf4c4ce93972a68ffb89fc8bd798
swapnilraut3 <swapnil.purandar@gmail.com>
2020-04-20 21:22:03 +0530

    feat: make property for won attribute

--- global_store.py
+++ global_store.py
@@ -3,19 +3,33 @@ class Global_store():
     This class stores Global constants
 
     Attributes:
-
-    total_lives (int) : the count of total lives a player holds
-    lost_lives (int): the count of lives lost by player
-    correct_guess (int): the count of correct guess made by player
-    won (boolean): boolean flag to mark victory of player
+        total_lives (int) :     the count of total lives a player holds
+        lost_lives (int):       the count of lives lost by player
+        correct_guess (int):    the count of correct guess made by player
+        won (boolean):          boolean flag to mark victory of player
     '''
 
-    def __init__(self, total_lives=7):
+    def __init__(self, total_lives=7, lost_lives=0, correct_guess=0, won=False):
         super().__init__()
         self._total_lives = total_lives
         self._lost_lives = 0
         self._correct_guess = 0
         self._won = False
+
+    @property
+    def won(self):
+        '''returns boolean flag to mark victory of player
+        true: win
+        false: loose'''
+        return self._won
+
+    @won.setter
+    def won(self, value):
+        '''set flag to mark victory of player
+        true: win
+        false: loose
+        '''
+        self._won = value
 
     @property
     def lost_lives(self):
@@ -38,10 +52,14 @@ class Global_store():
 
     @property
     def total_lives(self):
+        '''the count of total lives a player holds
+        '''
         return self._total_lives
 
     @total_lives.setter
     def total_lives(self, value):
+        '''set the count of total lives a player holds
+        '''
         if value < self._total_lives:
             self._total_lives = value
         else:
