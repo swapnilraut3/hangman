9bdacc1d9e47354f0b9baa6bf54873548056685f
swapnilraut3 <swapnil.purandar@gmail.com>
2020-04-21 11:38:54 +0530

    feat: Use correct guess from global store

--- logic.py
+++ logic.py
@@ -2,36 +2,35 @@ from print_works import Print_works
 from global_store import Global_store
 
 if __name__ == "__main__":
-    myobj = Global_store()
+    store = Global_store()
     print_job = Print_works()
 
     lostLives = 0
-    correct_guess = 0
     mylist = []
     unpresent_letters = []
 
-    print_job.mask_secret_word(myobj.secret_word)
+    print_job.mask_secret_word(store.secret_word)
 
-    mylist = list(myobj.secret_word)
+    mylist = list(store.secret_word)
     print_job.render_hangman()
 
-    while lostLives != myobj.total_lives:
+    while lostLives != store.total_lives:
         print(print_job.masked_word)
         guess = input('Guess the letter: ')
 
         if guess in mylist:
             index = mylist.index(guess)
             mylist[index] = 'GUESSED'
-            correct_guess += 1
+            store.correct_guess += 1
             print_job.unmask_secret_letter(index, guess)
             print('Correct guess !!')
-            if len(mylist) == correct_guess:
-                myobj.won = True
+            if len(mylist) == store.correct_guess:
+                store.won = True
                 break
         else:
             lostLives += 1
             print(
-                f'Wrong guess !! Lives remaining: {myobj.total_lives-lostLives} out of {myobj.total_lives}')
+                f'Wrong guess !! Lives remaining: {store.total_lives-lostLives} out of {store.total_lives}')
             if lostLives == 1:
                 print_job.render_hangman(h='■')
             elif lostLives == 2:
@@ -53,10 +52,10 @@ if __name__ == "__main__":
             print(f'Wrong guessed letter: {unpresent_letters}')
 
     else:
-        myobj.won = False
+        store.won = False
 
-    if myobj.won:
+    if store.won:
         print('You won!!')
     else:
         print('You lost')
-        print(f'The correct word was: {myobj.secret_word}')
+        print(f'The correct word was: {store.secret_word}')
