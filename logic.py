88b165210550359b868075d2991e37b9185f751b
swapnilraut3 <swapnil.purandar@gmail.com>
2020-04-21 10:07:06 +0530

    feat: Combine global store into main logic This is first step to convert complete logic in class

--- logic.py
+++ logic.py
@@ -2,6 +2,85 @@ from words import bag_of_words
 import random
 from print_works import Print_works
 
+
+class Play():
+    '''
+    This class implements core Hangman logic
+
+    Attributes:
+        total_lives (int) :     the count of total lives a player holds
+        lost_lives (int):       the count of lives lost by player
+        correct_guess (int):    the count of correct guess made by player
+        won (boolean):          boolean flag to mark victory of player
+    '''
+
+    def __init__(self, total_lives=7, lost_lives=0, correct_guess=0, won=False):
+        '''constructor for Play class
+
+        Attributes:
+            total_lives (int) :     the count of total lives a player holds
+            lost_lives (int):       the count of lives lost by player
+            correct_guess (int):    the count of correct guess made by player
+            won (boolean):          boolean flag to mark victory of player
+        '''
+        super().__init__()
+        self._total_lives = total_lives
+        self._lost_lives = 0
+        self._correct_guess = 0
+        self._won = False
+
+    @property
+    def correct_guess(self):
+        '''the count of correct guess made by player
+        '''
+        return self._correct_guess
+
+    def correct_guess(self, value):
+        '''set count of correct guess made by player
+        '''
+        self._correct_guess = value
+
+    @property
+    def won(self):
+        '''returns boolean flag to mark victory of player
+        true: win
+        false: loose'''
+        return self._won
+
+    @won.setter
+    def won(self, value):
+        '''set flag to mark victory of player
+        true: win
+        false: loose
+        '''
+        self._won = value
+
+    @property
+    def lost_lives(self):
+        '''the count of lives lost by player
+        '''
+        return self._lost_lives
+
+    @lost_lives.setter
+    def lost_lives(self, value):
+        '''set the count of lives lost by player
+        '''
+        if value < self.lost_lives:
+            raise ValueError(
+                "New value of lost lives can't be less than older one")
+        elif value > self.total_lives:
+            raise ValueError(
+                f"You can't set lost lives more than {self.total_lives}")
+        else:
+            self._lost_lives = value
+
+    @property
+    def total_lives(self):
+        '''the count of total lives a player holds
+        '''
+        return self._total_lives
+
+
 total_lives = (7,)
 lostLives = 0
 correct_guess = 0
